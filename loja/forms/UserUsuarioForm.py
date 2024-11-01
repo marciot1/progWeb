3aacb72cc7f555b5a2b1c9b40599a5bf8a5f2f76
marcio <marcio.t@escolar.ifrn.edu.br>
2024-11-01 10:27:27 -0300

    atulizacao

--- loja/forms/UserUsuarioForm.py
+++ loja/forms/UserUsuarioForm.py
@@ -1,12 +1,14 @@
 from django.forms import ModelForm
 from django import forms
 from loja.models.Usuario import Usuario
+# Adicione a linha a seguir no import
+from django.contrib.auth.models import User
 
 class UserUsuarioForm(ModelForm):
     def __init__(self, *args, **kwargs):
-    super(UserUsuarioForm, self).__init__(*args, **kwargs)
+        super(UserUsuarioForm, self).__init__(*args, **kwargs)
     if self.instance and self.instance.perfil != 1:
-            del self.fields['perfil'] #deleta campo se o usuário não for adm
+        del self.fields['perfil'] #deleta campo se o usuário não for adm
         # Adicione esse trecho antes da classe meta
     class Meta:
         model = Usuario
@@ -22,4 +24,14 @@ class UserUsuarioForm(ModelForm):
         'perfil': forms.Select(attrs={'class': "form-control"}),
         'aniversario': forms.DateInput(attrs={'class':"form-control", "type": "date"})
 
+    }
+    class UserForm(ModelForm):
+        class Meta:
+            model = User
+            fields = ['username', 'email', 'first_name', 'last_name']
+            widgets = {
+            'username': forms.TextInput(attrs={'class': "form-control"}),
+            'email': forms.EmailInput(attrs={'class': "form-control"}),
+            'first_name': forms.TextInput(attrs={'class': "form-control"}),
+            'last_name': forms.TextInput(attrs={'class': "form-control"})
         }
--- loja/views/UsuarioView.py
+++ loja/views/UsuarioView.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from loja.models import Usuario
-from loja.forms.UserUsuarioForm import UserUsuarioForm
+from loja.forms.UserUsuarioForm import UserUsuarioForm, UserForm
 def list_usuario_view(request, id=None):
     # carrega somente usuarios, não inclui os admin
     usuarios = Usuario.objects.filter(perfil=2)
-    
     context = {
         'usuarios': usuarios
     }
@@ -13,7 +12,10 @@ def list_usuario_view(request, id=None):
 def edit_usuario_view(request):
     usuario = get_object_or_404(Usuario, user=request.user)
     usuarioForm = UserUsuarioForm(instance=usuario)
+    # Crie uma instância da classe UserForm
+    userForm = UserForm(instance=request.user)
     context = {
-        'usuarioForm': usuarioForm
+    'usuarioForm': usuarioForm,
+    'userForm': userForm
     }
     return render(request, template_name='usuario/usuario-edit.html', context=context, status=200)
