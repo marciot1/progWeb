95aca25575f43ca4dcee2d1baab1b86f1d8ab3d6
marcio <marcio.t@escolar.ifrn.edu.br>
2024-11-08 10:11:03 -0300

    atuliza

--- loja/forms/UserUsuarioForm.py
+++ loja/forms/UserUsuarioForm.py
@@ -5,17 +5,7 @@ from loja.models.Usuario import Usuario
 from django.contrib.auth.models import User
 # Adicione esse trecho antes da classe meta
 class UserUsuarioForm(ModelForm):
-    # Adicione o código da classe UserForm
-    class UserForm(ModelForm):
-        class Meta:
-        model = User
-        fields = ['username', 'email', 'first_name', 'last_name']
-        widgets = {
-            'username': forms.TextInput(attrs={'class': "form-control"}),
-            'email': forms.EmailInput(attrs={'class': "form-control"}),
-            'first_name': forms.TextInput(attrs={'class': "form-control"}),
-            'last_name': forms.TextInput(attrs={'class': "form-control"})
-        }
+
     def __init__(self, *args, **kwargs):
         super(UserUsuarioForm, self).__init__(*args, **kwargs)
         if self.instance and self.instance.perfil != 1: del self.fields['perfil']
@@ -30,3 +20,15 @@ class UserUsuarioForm(ModelForm):
         # Usamos exclude para excluir campos específicos do sistema
         # exclude = ['']
         widgets = {'user': forms.HiddenInput(),'perfil': forms.Select(attrs={'class': "form-control"}),'aniversario': forms.DateInput(attrs={'class':"form-control", "type": "date"})}
+
+        # Adicione o código da classe UserForm
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': "form-control"}),
+            'email': forms.EmailInput(attrs={'class': "form-control"}),
+            'first_name': forms.TextInput(attrs={'class': "form-control"}),
+            'last_name': forms.TextInput(attrs={'class': "form-control"})
+        }    
--- loja/views/UsuarioView.py
+++ loja/views/UsuarioView.py
@@ -23,36 +23,33 @@ def edit_usuario_view(request):
         userForm = UserForm(request.POST, instance=request.user)
         # Verifica se o e-mail que o usuário está tentando utilizar
         # em seu perfil já existe em outro perfil
-        verifyEmail = Usuario.objects.filter(user__email=request.POST['email']).exclude(user__id=request.user.
-        id).first()
-                emailUnused = verifyEmail is None
-        else:
-            usuarioForm = UserUsuarioForm(instance=usuario)
-            userForm = UserForm(instance=request.user)
+        verifyEmail = Usuario.objects.filter(user__email=request.POST['email']).exclude(user__id=request.user.id).first()
+        emailUnused = verifyEmail is None
+    else:
+        usuarioForm = UserUsuarioForm(instance=usuario)
+        userForm = UserForm(instance=request.user)
         if usuarioForm.is_valid() and userForm.is_valid() and emailUnused:
             usuarioForm.save()
             userForm.save()
             # Adicione a linha a seguir
             message = { 'type': 'success', 'text': 'Dados atualizados com sucesso' }
             # Adicione as linhas a seguir
-            else:
-                # Aqui verificamos se é do tipo post, para que na primeira vez que a página
-                carregar a mensagem não apareça, já que no primeiro carregamento não enviamos um post, o
-                form é dado como inválido e entra aqui.
-                if request.method == 'POST':
-                    if emailUnused:
-                        # Se o e-mail não está em uso tiver algum dado inválido.
+        else:
+            # Aqui verificamos se é do tipo post, para que na primeira vez que a página
+            if request.method == 'POST':
+                if emailUnused:
+                    # Se o e-mail não está em uso tiver algum dado inválido.
 
-                        message = { 'type': 'danger', 'text': 'Dados inválidos' }
-                    else:
-                        # Se o e-mail já está em uso por outra pessoa.
-                        message = { 'type': 'warning', 'text': 'E-mail já usado' }
+                    message = { 'type': 'danger', 'text': 'Dados inválidos' }
+                else:
+                    # Se o e-mail já está em uso por outra pessoa.
+                    message = { 'type': 'warning', 'text': 'E-mail já usado' }
 
-                        # Até aqui
-                        # Adicione a chave message a seguir
+                    # Até aqui
+                    # Adicione a chave message a seguir
     context = {
-    'usuarioForm': usuarioForm,
-    'userForm': userForm
-    'message': message
+        'usuarioForm': usuarioForm,
+        'userForm': userForm,
+        'message': message
     }
     return render(request, template_name='usuario/usuario-edit.html', context=context, status=200)
