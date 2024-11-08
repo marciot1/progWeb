4d7f37db918f64c77adcff3394b874b8171e64d0
Marcio Targino Do Nascimento <105668116+marciot1@users.noreply.github.com>
2024-11-08 08:25:54 -0300

    capitulo 9

--- loja/forms/UserUsuarioForm.py
+++ loja/forms/UserUsuarioForm.py
@@ -3,12 +3,22 @@ from django import forms
 from loja.models.Usuario import Usuario
 # Adicione a linha a seguir no import
 from django.contrib.auth.models import User
-
+# Adicione esse trecho antes da classe meta
 class UserUsuarioForm(ModelForm):
+    # Adicione o código da classe UserForm
+    class UserForm(ModelForm):
+        class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': "form-control"}),
+            'email': forms.EmailInput(attrs={'class': "form-control"}),
+            'first_name': forms.TextInput(attrs={'class': "form-control"}),
+            'last_name': forms.TextInput(attrs={'class': "form-control"})
+        }
     def __init__(self, *args, **kwargs):
         super(UserUsuarioForm, self).__init__(*args, **kwargs)
-    if self.instance and self.instance.perfil != 1:
-        del self.fields['perfil'] #deleta campo se o usuário não for adm
+        if self.instance and self.instance.perfil != 1: del self.fields['perfil']
         # Adicione esse trecho antes da classe meta
     class Meta:
         model = Usuario
@@ -19,19 +29,4 @@ class UserUsuarioForm(ModelForm):
         # fields = ['pub_date', 'headline', 'content', 'reporter']
         # Usamos exclude para excluir campos específicos do sistema
         # exclude = ['']
-        widgets = {
-        'user': forms.HiddenInput(),
-        'perfil': forms.Select(attrs={'class': "form-control"}),
-        'aniversario': forms.DateInput(attrs={'class':"form-control", "type": "date"})
-
-    }
-    class UserForm(ModelForm):
-        class Meta:
-            model = User
-            fields = ['username', 'email', 'first_name', 'last_name']
-            widgets = {
-            'username': forms.TextInput(attrs={'class': "form-control"}),
-            'email': forms.EmailInput(attrs={'class': "form-control"}),
-            'first_name': forms.TextInput(attrs={'class': "form-control"}),
-            'last_name': forms.TextInput(attrs={'class': "form-control"})
-        }
+        widgets = {'user': forms.HiddenInput(),'perfil': forms.Select(attrs={'class': "form-control"}),'aniversario': forms.DateInput(attrs={'class':"form-control", "type": "date"})}
--- loja/urls/UsuarioUrls.py
+++ loja/urls/UsuarioUrls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from loja.views.UsuarioView import list_usuario_view, edit_usuario_view
 urlpatterns = [
-    path("", list_usuario_view, name='usuario'),
+    path("", list_usuario_view, name='usuarios'),
     path("edit", edit_usuario_view, name='edit_usuario'),
 ]
